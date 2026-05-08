bd962b9d61fbc8e8c6215a8f9e766372579c4db8
whaleshine <whaleshine98@gmail.com>
2026-05-08 12:47:03 +0530

    fix: update response message in root route to "Hello World!"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.get((req, res) => res.send("Hello"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
