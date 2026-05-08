323e38fed2ca6dde5d24993fb553bea65c06562f
whaleshine <whaleshine98@gmail.com>
2026-05-08 11:20:39 +0530

    added initial files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get((req, res) => res.send("Hello"));
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
